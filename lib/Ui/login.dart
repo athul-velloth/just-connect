c5aaef77b0140499f018f80793a9aa536d2053eb
kishan.choudhary <kishan.choudhary@6dtech.co.in>
2024-12-13 16:59:39 +0530

    working on login type

--- lib/Ui/login.dart
+++ lib/Ui/login.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -22,6 +20,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  String _userType = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,17 +36,54 @@ class _LoginState extends State<Login> {
             child: Column(
               children: [
                 SizedBox(height: SizeConstant.getHeightWithScreen(100)),
-                Text("Login", style: TextStyle(
-                  color: ColorConstant.black,
-                  fontSize: SizeConstant.largeFont
-                ),),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                      color: ColorConstant.black,
+                      fontSize: SizeConstant.largeFont),
+                ),
                 SizedBox(height: SizeConstant.getHeightWithScreen(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _userType = 'Owner';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _userType == 'Owner'
+                            ? Colors.blue
+                            : Colors.white, // Change color
+                        foregroundColor:
+                            _userType == 'Owner' ? Colors.white : Colors.black,
+                      ),
+                      child:  Text('Owner'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _userType = 'Worker';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _userType == 'Worker'
+                            ? Colors.blue
+                            : Colors.white, // Change color
+                        foregroundColor: _userType == 'Worker'
+                            ? Colors.white
+                            : Colors.black, // Change color
+                      ),
+                      child: Text('Worker'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
                 CommonTextInputField(
-                  textInputType: TextInputType.number,
+                  textInputType: TextInputType.emailAddress,
                   showLabel: false,
-                  height:
-                  SizeConstant.getHeightWithScreen(
-                      50),
+                  height: SizeConstant.getHeightWithScreen(50),
                   controller: emailController,
                   hintText: "Enter Email",
                   isAteriskRequired: false,
@@ -55,17 +91,15 @@ class _LoginState extends State<Login> {
                   onChanged: (p0) {
                     setState(() {});
                   },
+                 
                 ),
                 SizedBox(height: SizeConstant.getHeightWithScreen(10)),
                 CommonTextInputField(
-                  textInputType: TextInputType.number,
+                  textInputType: TextInputType.visiblePassword,
                   showLabel: false,
-                  height:
-                  SizeConstant.getHeightWithScreen(
-                      50),
+                  height: SizeConstant.getHeightWithScreen(50),
                   controller: passwordController,
                   hintText: "Enter Password",
-            
                   isAteriskRequired: false,
                   enableInteractiveSelection: false,
                   onChanged: (p0) {
@@ -75,27 +109,57 @@ class _LoginState extends State<Login> {
                 SizedBox(height: SizeConstant.getHeightWithScreen(10)),
                 CommonButton(
                     bgColor: ColorConstant.outletButtonColor,
-                    btnHeight:
-                    SizeConstant.getHeightWithScreen(48),
+                    btnHeight: SizeConstant.getHeightWithScreen(48),
                     onTap: () {
-                        String pattern =
-                        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
-                        RegExp regex = RegExp(pattern);
-                      if (!regex
-                          .hasMatch(emailController.text.trim())) {
-                        Get.to(() => const Home());
+                      String pattern =
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+                      RegExp regex = RegExp(pattern);
+                      
+                      final username = emailController.text;
+                      final password = passwordController.text;
+                      if (username.isEmpty || password.isEmpty) {
+                        // Show error if fields are empty
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text(
+                                  'Please enter both email and password')),
+                        );
+                      } else if (!regex.hasMatch(emailController.text.trim())) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text(
+                                  'Please enter valid email')),
+                        );
+                      }
+                      
+                      else if (_userType.isEmpty) {
+                        // Show error if user type is not selected
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please select user type')),
+                        );
+                      } else {
+                        // Navigate based on selected user type
+                        if (_userType == 'Owner') {
+                          Get.to(() =>
+                              const Home()); // Navigate to Owner home page
+                        } else if (_userType == 'Worker') {
+                          Get.to(() =>
+                              const Home()); // Navigate to Worker home page
+                        }
                       }
                     },
                     label: 'Login'),
                 SizedBox(height: SizeConstant.getHeightWithScreen(10)),
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Get.to(() => const SignUp());
                   },
-                  child: Text("Sign Up", style: TextStyle(
-                      color: ColorConstant.primaryColor,
-                      fontSize: SizeConstant.largeFont
-                  ),),
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                        color: ColorConstant.primaryColor,
+                        fontSize: SizeConstant.largeFont),
+                  ),
                 ),
               ],
             ),
--- lib/Ui/signup.dart
+++ lib/Ui/signup.dart
@@ -1,11 +1,12 @@
-
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:justconnect/Ui/login.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 import '../constants/color_constants.dart';
 import '../constants/size_constants.dart';
 import '../widget/commontextInputfield.dart';
@@ -22,6 +23,47 @@ class _SignUpState extends State<SignUp> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController faltNoController = TextEditingController();
+  final TextEditingController mobileNoController = TextEditingController();
+  final TextEditingController typeController = TextEditingController();
+  String _userType = '';
+  File? _selfieImage; // To store the captured image
+  final ImagePicker _picker = ImagePicker();
+  Future<void> _takeSelfie() async {
+    final XFile? image = await _picker.pickImage(
+      source: ImageSource.camera, // Use camera for selfie
+      preferredCameraDevice: CameraDevice.front, // Front camera for selfies
+    );
+    if (image != null) {
+      setState(() {
+        _selfieImage = File(image.path); // Store the image file
+      });
+    }
+  }
+
+  Future<void> _requestCameraPermission() async {
+    var status = await Permission.camera.status;
+
+    if (status.isPermanentlyDenied) {
+      openAppSettings(); // This function navigates the user to app settings
+      return;
+    }
+    if (!status.isGranted) {
+      // Request the permission if not already granted
+      status = await Permission.camera.request();
+    }
+
+    if (status.isGranted) {
+      _takeSelfie(); // Call the function to capture an image if permission is granted
+    } else if (status.isDenied || status.isPermanentlyDenied) {
+      // Show a dialog or handle permission denial
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text("Camera permission is required to take selfies.")),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,18 +78,74 @@ class _SignUpState extends State<SignUp> {
             padding: const EdgeInsets.only(bottom: 10),
             child: Column(
               children: [
-                SizedBox(height: SizeConstant.getHeightWithScreen(100)),
-                Text("Sign Up", style: TextStyle(
-                    color: ColorConstant.black,
-                    fontSize: SizeConstant.largeFont
-                ),),
+                SizedBox(height: SizeConstant.getHeightWithScreen(50)),
+                Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: ColorConstant.black,
+                      fontSize: SizeConstant.largeFont),
+                ),
                 SizedBox(height: SizeConstant.getHeightWithScreen(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _userType = 'Owner';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _userType == 'Owner'
+                            ? Colors.blue
+                            : Colors.white, // Change color
+                        foregroundColor:
+                            _userType == 'Owner' ? Colors.white : Colors.black,
+                      ),
+                      child: Text('Owner'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _userType = 'Worker';
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _userType == 'Worker'
+                            ? Colors.blue
+                            : Colors.white, // Change color
+                        foregroundColor: _userType == 'Worker'
+                            ? Colors.white
+                            : Colors.black, // Change color
+                      ),
+                      child: Text('Worker'),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 50, // Size of the avatar
+                  backgroundImage: _selfieImage != null
+                      ? FileImage(_selfieImage!) // Display captured selfie
+                      : null,
+                  child: _selfieImage == null
+                      ? const Icon(
+                          Icons.camera_alt,
+                          size: 40,
+                          color: Colors.grey,
+                        )
+                      : null, // Show a camera icon if no selfie is taken
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _requestCameraPermission, // Trigger selfie capture
+                  child: const Text('Take Selfie'),
+                ),
+                const SizedBox(height: 20),
                 CommonTextInputField(
                   textInputType: TextInputType.number,
                   showLabel: false,
-                  height:
-                  SizeConstant.getHeightWithScreen(
-                      50),
+                  height: SizeConstant.getHeightWithScreen(50),
                   controller: nameController,
                   hintText: "Enter Name",
                   isAteriskRequired: false,
@@ -60,9 +158,7 @@ class _SignUpState extends State<SignUp> {
                 CommonTextInputField(
                   textInputType: TextInputType.number,
                   showLabel: false,
-                  height:
-                  SizeConstant.getHeightWithScreen(
-                      50),
+                  height: SizeConstant.getHeightWithScreen(50),
                   controller: emailController,
                   hintText: "Enter Email",
                   isAteriskRequired: false,
@@ -75,12 +171,52 @@ class _SignUpState extends State<SignUp> {
                 CommonTextInputField(
                   textInputType: TextInputType.number,
                   showLabel: false,
-                  height:
-                  SizeConstant.getHeightWithScreen(
-                      50),
+                  height: SizeConstant.getHeightWithScreen(50),
+                  controller: mobileNoController,
+                  hintText: "Enter mobile no.",
+                  isAteriskRequired: false,
+                  enableInteractiveSelection: false,
+                  onChanged: (p0) {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(height: SizeConstant.getHeightWithScreen(10)),
+                CommonTextInputField(
+                  textInputType: TextInputType.number,
+                  showLabel: false,
+                  height: SizeConstant.getHeightWithScreen(50),
+                  controller: faltNoController,
+                  hintText: "Enter Flat No.",
+                  isAteriskRequired: false,
+                  enableInteractiveSelection: false,
+                  onChanged: (p0) {
+                    setState(() {});
+                  },
+                ),
+                SizedBox(
+                    height: SizeConstant.getHeightWithScreen(
+                        _userType == "Owner" ? 0 : 10)),
+                _userType == "Owner"
+                    ? const SizedBox()
+                    : CommonTextInputField(
+                        textInputType: TextInputType.text,
+                        showLabel: false,
+                        height: SizeConstant.getHeightWithScreen(50),
+                        controller: typeController,
+                        hintText: "Enter Type of Job",
+                        isAteriskRequired: false,
+                        enableInteractiveSelection: false,
+                        onChanged: (p0) {
+                          setState(() {});
+                        },
+                      ),
+                SizedBox(height: SizeConstant.getHeightWithScreen(10)),
+                CommonTextInputField(
+                  textInputType: TextInputType.number,
+                  showLabel: false,
+                  height: SizeConstant.getHeightWithScreen(50),
                   controller: passwordController,
                   hintText: "Enter Password",
-
                   isAteriskRequired: false,
                   enableInteractiveSelection: false,
                   onChanged: (p0) {
@@ -90,25 +226,25 @@ class _SignUpState extends State<SignUp> {
                 SizedBox(height: SizeConstant.getHeightWithScreen(10)),
                 CommonButton(
                     bgColor: ColorConstant.outletButtonColor,
-                    btnHeight:
-                    SizeConstant.getHeightWithScreen(48),
+                    btnHeight: SizeConstant.getHeightWithScreen(48),
                     onTap: () {
                       String pattern =
                           r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
                       RegExp regex = RegExp(pattern);
-                      if (!regex
-                          .hasMatch(emailController.text.trim())) {}
+                      if (!regex.hasMatch(emailController.text.trim())) {}
                     },
                     label: 'Sign Up'),
                 SizedBox(height: SizeConstant.getHeightWithScreen(10)),
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Get.to(() => const Login());
                   },
-                  child: Text("Login", style: TextStyle(
-                      color: ColorConstant.primaryColor,
-                      fontSize: SizeConstant.largeFont
-                  ),),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: ColorConstant.primaryColor,
+                        fontSize: SizeConstant.largeFont),
+                  ),
                 ),
               ],
             ),
